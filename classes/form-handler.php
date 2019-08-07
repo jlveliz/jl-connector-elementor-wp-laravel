d46230c8213515f0f2502edae51aebb252c4f4a2
Jorge Veliz <jorgeconsalvacion@gmail.com>
2019-08-06 19:42:19 -0500

    valida el nombre del formulario

--- classes/form-handler.php
+++ classes/form-handler.php
@@ -27,50 +27,56 @@
             add_action( 'elementor_pro/forms/new_record', function( $record, $handler ) {
                 //make sure its our form
                 $form_name = $record->get_form_settings( 'form_name' );
-            
-                $end_point = get_option('jl_field_endpoint');
-                $raw_fields = $record->get( 'sent_data' );
                 
-                
-                $url = $end_point . $this->route;
-                $args = $this->getHeaders();
-
-                
-                /*
-                    TOCAR CON CAUTELA 
-                */ 
-                //children
-                $args['body'] = [
-                    'name' => $raw_fields['nombre_hijo'],
-                    'last_name' => $raw_fields['apellido_hijo'],
-                    'age' => $raw_fields['age'],
-
-                ];
-                //representant
-                $args['body']['representant'] = [
-                    'name' => $raw_fields['nombre_representante'],
-                    'last_name' => $raw_fields['apellido_representante'],
-                    'email' => $raw_fields['email'],
-                    'mobile' => $raw_fields['celular'],
-                ];
-
-                $args['body']['enrollment'] = [
-                    'field_id' => $raw_fields['field'],
-                    'day' => $raw_fields['day'],
-                    'hour' => $raw_fields['hour']
-                ];
-               
-                
-                $response = wp_remote_post($url,$args);
-
-
-                $message = wp_remote_retrieve_body( $response );
-
-                
-                if($response['response']['code'] == 401 || $response['response']['code'] == 500) {
-                    $handler->add_error_message($message)->send();
+                if($form_name == 'Registro') {
+                    $end_point = get_option('jl_field_endpoint');
+                    $raw_fields = $record->get( 'sent_data' );
                     
+                    
+                    $url = $end_point . $this->route;
+                    $args = $this->getHeaders();
+    
+                    
+                    /*
+                        TOCAR CON CAUTELA 
+                    */ 
+                    //children
+                    $args['body'] = [
+                        'name' => $raw_fields['nombre_hijo'],
+                        'last_name' => $raw_fields['apellido_hijo'],
+                        'age' => $raw_fields['age'],
+    
+                    ];
+                    //representant
+                    $args['body']['representant'] = [
+                        'name' => $raw_fields['nombre_representante'],
+                        'last_name' => $raw_fields['apellido_representante'],
+                        'email' => $raw_fields['email'],
+                        'mobile' => $raw_fields['celular'],
+                    ];
+    
+                    $args['body']['enrollment'] = [
+                        'field_id' => $raw_fields['field'],
+                        'day' => $raw_fields['day'],
+                        'hour' => $raw_fields['hour']
+                    ];
+                   
+                    
+                    $response = wp_remote_post($url,$args);
+    
+    
+                    $message = wp_remote_retrieve_body( $response );
+    
+                    
+                    if($response['response']['code'] == 401 || $response['response']['code'] == 500) {
+                        $handler->add_error_message($message)->send();
+                        
+                    }
+                } else {
+
                 }
+            
+               
 
             }, 10, 2 );
 
